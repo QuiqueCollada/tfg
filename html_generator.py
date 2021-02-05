45cd679c2cd65566bdd4eb4861eaa07e52ab997c
QuiqueCollada <e.collada@usp.ceu.es>
2021-02-05 13:18:42 +0100

    Produce HTML básico pero no PDF

--- html_generator.py
+++ html_generator.py
@@ -1,8 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
+import pdfkit
+
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template("myreport.html")
 
-template_vars = {"title" : "Sales Funnel Report - National",
-                 "national_pivot_table": sales_report.to_html()}
+template_vars = {"title" : "Lesión X - Paciente X","imagen" : "img1.jpg","leyenda" : "Lesión talámica","Autor" : "Enrique"}
 
 html_out = template.render(template_vars)
+#print(html_out)
+
+Html_file = open("report.html","w")
+Html_file.write(html_out)
+Html_file.close()
+
+pdfkit.from_string('html_out', 'report1.pdf')
+pdfkit.from_file('report.html', 'report.pdf')
+
+
