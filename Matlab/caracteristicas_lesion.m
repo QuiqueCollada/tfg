77f4d10f37e7c0d090b4436d981dd314d4e63ceb
QuiqueCollada <e.collada@usp.ceu.es>
2021-04-08 18:29:44 +0200

    Error al obtener el volumen

--- Matlab/caracteristicas_lesion.m
+++ Matlab/caracteristicas_lesion.m
@@ -3,6 +3,8 @@ clc; close all; clear;
 path = ['/Users/enrique/GitHubProjects/tfg/Img_lesion/' 'Lesion_T2_PO_1D_W.nii.gz']
 hdr = load_untouch_nii(path);
 
-vol = sum(hdr.mask(:));
+vol = sum(hdr.vol(:));
+hdr.pixdim
+prod(hdr.pixdim(1:3)) * vol
 
 
