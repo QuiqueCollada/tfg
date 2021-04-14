c93ae892c2e46073726ab2bba5b962bdffcb2215
QuiqueCollada <e.collada@usp.ceu.es>
2021-04-14 16:41:25 +0200

    Nueva función load_nifti.m

--- Matlab/caracteristicas_lesion.m
+++ Matlab/caracteristicas_lesion.m
@@ -1,10 +1,11 @@
 clc; close all; clear;
 
 path = ['/Users/enrique/GitHubProjects/tfg/Img_lesion/' 'Lesion_T2_PO_1D_W.nii.gz']
-hdr = load_untouch_nii(path);
+hdr = load_nifti(path);
 
 vol = sum(hdr.vol(:));
 hdr.pixdim
 prod(hdr.pixdim(1:3)) * vol
 
+s = regionprops(hdr,'centroid');
 
