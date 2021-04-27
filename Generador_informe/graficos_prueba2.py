b91bb60ab4e726ba16516cb8f5b0466f95081d67
QuiqueCollada <e.collada@usp.ceu.es>
2021-04-27 18:30:22 +0200

    Ordenar valores x e y para scatterplot

--- Generador_informe/graficos_prueba2.py
+++ Generador_informe/graficos_prueba2.py
@@ -1,13 +1,23 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
+
 
 plt.style.use('seaborn')
 
-df = pd.read_csv("caracteristicas_mejoria2.csv")
+df = pd.read_csv("caracteristicas_mejoria4.csv")
 volumen = df['volumen']
 mejoria = df['mejoria']
 
-plt.scatter(mejoria,volumen, edgecolor='black',linewidth=1,alpha=0.75)
+# Ordeno los valores
+x = np.sort(volumen.values.flatten())
+y = np.sort(mejoria.values.flatten())
+
+plt.scatter(x,y, edgecolor='black',linewidth=1,alpha=0.75)
+
+# Límites de los ejes x e y
+plt.xlim(100, 300)
+plt.ylim(0, 1)
 
 plt.title("Relación entre el volumen y la mejoría clínica")
 plt.xlabel('Mejoría')
