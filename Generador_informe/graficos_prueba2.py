a6bced28b60bf59c9a3e1deebc00aaf3ff4592da
QuiqueCollada <e.collada@usp.ceu.es>
2021-05-05 17:53:42 +0200

    Primer gráfico en png

--- Generador_informe/graficos_prueba2.py
+++ Generador_informe/graficos_prueba2.py
@@ -5,7 +5,8 @@ import numpy as np
 
 plt.style.use('seaborn')
 
-df = pd.read_csv("caracteristicas_mejoria5.csv")
+df = pd.read_csv("caracteristicas_mejoria_americano.csv", sep=";")
+df = df.apply(pd.to_numeric)
 volumen = df['volumen']
 mejoria = df['mejoria']
 
@@ -17,16 +18,17 @@ plt.scatter(x,y, edgecolor='black',linewidth=1,alpha=0.75)
 
 # Límites de los ejes x e y
 #plt.xlim(100, 300)
-#plt.ylim(0, 1)
+plt.ylim(0, 1)
 
 # Establezco las marcas de los ejes x e y
 plt.xticks(range(200,300,10))
-plt.yticks(range(0,100,10))
+#plt.yticks(range(0,100,10))
 
-plt.title("Relación entre el volumen y la mejoría clínica")
-plt.xlabel('Mejoría')
-plt.ylabel('Volumen')
+plt.title("Relación entre el volumen de la lesión y la mejoría clínica")
+plt.ylabel('Mejoría (%)')
+plt.xlabel('Volumen (mm3)')
 
 plt.tight_layout()
 
+plt.savefig('./Graficos/figure1.png')
 plt.show()
